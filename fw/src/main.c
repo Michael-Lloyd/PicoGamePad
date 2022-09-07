c9d061075fa6be3f3f248834cb003789a30c1b2d
Michael-Lloyd <michaelwoodgerlloyd@gmail.com>
2022-09-08 07:11:26 +1000

    Boilerplate

---
+++ fw/src/main.c
@@ -0,0 +1,16 @@
+#include "pico/stdlib.h"
+
+int main(void) { 
+    
+    const uint32_t LED_PIN = PICO_DEFAULT_LED_PIN; 
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while(1) { 
+        gpio_put(LED_PIN, 1); 
+        sleep_ms(250);
+        gpio_put(LED_PIN 0); 
+        sleep_ms(250);
+    } 
+
+    return 0;
+} 
