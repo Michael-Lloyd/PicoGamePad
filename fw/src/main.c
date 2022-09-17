dee9ee49f5a3e17f3b73ce66c0d30ca8acc38def
Michael-Lloyd <michaelwoodgerlloyd@gmail.com>
2022-09-18 00:17:36 +1000

    Refactoring for Xinput

---
+++ fw/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+#include "pico/stdlib.h" 
+#include "pico/time.h" 
+
+#include "tusb.h"
+
+#include "device/usbd_pvt.h"
+
+#include "XInputGamepad.h" 
+
+
